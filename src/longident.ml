ocaml-ppx/ppxlib

Fix parsing of ')' as a valid lident
Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

src/longident.ml
@@ -60,11 +60,10 @@ let parse s =
   let invalid () =
     invalid_arg (Printf.sprintf "Ppxlib.Longident.parse: %S" s)
   in
-  match String.index s '(' with
-  | None -> parse_simple  s
-  | Some l -> match String.rindex s ')' with
-    | None -> invalid ()
-    | Some r ->
+  match String.index s '(', String.rindex s ')' with
+  | None, None -> parse_simple  s
+  | None, _ | _, None -> invalid ()
+  | Some l, Some r ->
       if Int.( r <> String.length s - 1 ) then invalid ();
       let group = if Int.(r = l + 1) then "()" else
           String.strip (String.sub s ~pos:(l+1) ~len:(r-l-1))

test/base/test.ml
@@ -115,7 +115,7 @@ Exception: Invalid_argument "Ppxlib.Longident.parse: \"A.B(C)\"".
 
 let _ = convert_longident ")"
 [%%expect{|
-- : string * longident = ("( ) )", Ppxlib.Longident.Lident ")")
+Exception: Invalid_argument "Ppxlib.Longident.parse: \")\"".
 |}]
 
 let _ = Ppxlib.Code_path.(file_path @@ top_level ~file_path:"dir/main.ml")

ocaml-ppx/ppx

src/longident.ml
@@ -56,13 +56,11 @@ let parse s =
   let invalid () =
     invalid_arg (Printf.sprintf "Ppx.Longident.parse: %S" s)
   in
-  match String.index s '(' with
-  | None -> parse_simple  s
-  | Some l -> match String.rindex_opt s ')' with
-    | None -> invalid ()
-    | Some r ->
-      if r <> String.length s - 1 then
-        invalid_arg "Ppx.Longident.parse";
+  match String.index s '(', String.rindex_opt s ')' with
+  | None, None -> parse_simple  s
+  | None, _ | _, None -> invalid ()
+  | Some l, Some r ->
+      if r <> String.length s - 1 then invalid ();
       let group = if r = l + 1 then "()" else
           String.trim (String.sub s ~pos:(l+1) ~len:(r-l-1))
       in

test/base/test.ml
@@ -114,7 +114,7 @@ Exception: Invalid_argument "Ppx.Longident.parse: \"A.B(C)\"".
 
 let _ = convert_longident ")"
 [%%expect{|
-- : string * longident = ("( ) )", Ppx.Longident.Lident ")")
+Exception: Invalid_argument "Ppx.Longident.parse: \")\"".
 |}]
 
 let _ = Ppx.Code_path.(file_path @@ top_level ~file_path:"dir/main.ml")