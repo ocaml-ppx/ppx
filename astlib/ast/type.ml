fa8c8924f411326fbb28adaf5ad83f074cb2a68f
Carl Eastlund <ceastlund@janestreet.com>
2019-10-03 17:04:00 -0400

    Rewrite grammar to mirror Ocaml types used in parsetree.

--- astlib/ast/type.ml
+++ astlib/ast/type.ml
@@ -1,25 +1,51 @@
-type kind =
-  { kind_name : string
-  ; clauses   : clause list
-  }
-
-and clause =
-  { clause_name : string
-  ; fields      : field list
-  }
-
-and field =
-  { field_name : string
-  ; data       : data
-  }
-
-and data =
-  | Kind of string
+type 'var structural =
+  | Var of 'var
+  | Inst of 'var inst
+  | Named of string
   | Bool
+  | Int
   | Char
   | String
   | Location
-  | List   of data
-  | Option of data
+  | List of 'var structural
+  | Option of 'var structural
+  | Tuple of 'var tuple
 
-type t = kind list
+and 'var tuple = 'var structural list
+
+and 'var inst =
+  { inst_poly : string
+  ; inst_args : 'var structural list
+  }
+
+type 'var field =
+  { field_name : string
+  ; field_type : 'var structural
+  }
+
+type 'var record = 'var field list
+
+type 'var clause =
+  | Inline_tuple of 'var tuple
+  | Inline_record of 'var record
+
+type 'var variant = 'var clause list
+
+type 'var nominal =
+  | Alias of 'var structural
+  | Record of 'var record
+  | Variant of 'var variant
+
+type 'var named =
+  { decl_name : string
+  ; decl_vars : 'var list
+  ; decl_body : 'var nominal
+  }
+
+type nothing = |
+
+type decl =
+  | Poly of string named
+  | Mono of nothing named
+
+type t = decl list
