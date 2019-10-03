ocaml-ppx/ppxlib

Include argument in Longident.parse error message
Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

src/longident.ml
@@ -57,18 +57,20 @@ let parse_simple s =
 
 (* handle ["A.B.(+.+)"] or ["Vec.(.%.()<-)"] *)
 let parse s =
+  let invalid () =
+    invalid_arg (Printf.sprintf "Ppxlib.Longident.parse: %S" s)
+  in
   match String.index s '(' with
   | None -> parse_simple  s
   | Some l -> match String.rindex s ')' with
-    | None -> invalid_arg "Ppxlib.Longident.parse"
+    | None -> invalid ()
     | Some r ->
-      if Int.( r <> String.length s - 1 ) then
-        invalid_arg "Ppxlib.Longident.parse";
+      if Int.( r <> String.length s - 1 ) then invalid ();
       let group = if Int.(r = l + 1) then "()" else
           String.strip (String.sub s ~pos:(l+1) ~len:(r-l-1))
       in
       if Int.(l = 0) then Lident group
-      else if Char.(s.[l - 1] <> '.') then invalid_arg "Ppxlib.Longident.parse"
+      else if Char.(s.[l - 1] <> '.') then invalid ()
       else
         let before = String.sub s ~pos:0 ~len:(l-1) in
         match String.split before ~on:'.' with

test/base/test.ml
@@ -105,7 +105,7 @@ let _ = convert_longident "Base.( land )"
 
 let _ = convert_longident "A(B)"
 [%%expect{|
-Exception: Invalid_argument "Ppxlib.Longident.parse".
+Exception: Invalid_argument "Ppxlib.Longident.parse: \"A(B)\"".
 Raised at file "stdlib.ml", line 34, characters 25-45
 Called from file "test/base/test.ml", line 3, characters 15-37
 Called from file "toplevel/toploop.ml", line 180, characters 17-56
@@ -113,7 +113,7 @@ Called from file "toplevel/toploop.ml", line 180, characters 17-56
 
 let _ = convert_longident "A.B(C)"
 [%%expect{|
-Exception: Invalid_argument "Ppxlib.Longident.parse".
+Exception: Invalid_argument "Ppxlib.Longident.parse: \"A.B(C)\"".
 Raised at file "stdlib.ml", line 34, characters 25-45
 Called from file "test/base/test.ml", line 3, characters 15-37
 Called from file "toplevel/toploop.ml", line 180, characters 17-56

ocaml-ppx/ppx

src/longident.ml
@@ -53,10 +53,13 @@ let parse_simple s =
 
 (* handle ["A.B.(+.+)"] or ["Vec.(.%.()<-)"] *)
 let parse s =
+  let invalid () =
+    invalid_arg (Printf.sprintf "Ppx.Longident.parse: %S" s)
+  in
   match String.index s '(' with
   | None -> parse_simple  s
   | Some l -> match String.rindex_opt s ')' with
-    | None -> invalid_arg "Ppx.Longident.parse"
+    | None -> invalid ()
     | Some r ->
       if r <> String.length s - 1 then
         invalid_arg "Ppx.Longident.parse";
@@ -64,7 +67,7 @@ let parse s =
           String.trim (String.sub s ~pos:(l+1) ~len:(r-l-1))
       in
       if l = 0 then Lident group
-      else if s.[l - 1] <> '.' then invalid_arg "Ppx.Longident.parse"
+      else if s.[l - 1] <> '.' then invalid ()
       else
         let before = String.sub s ~pos:0 ~len:(l-1) in
         match String.split before ~on:'.' with

test/base/test.ml
@@ -104,7 +104,7 @@ let _ = convert_longident "Base.( land )"
 
 let _ = convert_longident "A(B)"
 [%%expect{|
-Exception: Invalid_argument "Ppx.Longident.parse".
+Exception: Invalid_argument "Ppx.Longident.parse: \"A(B)\"".
 Raised at file "stdlib.ml", line 34, characters 25-45
 Called from file "test/base/test.ml", line 3, characters 15-37
 Called from file "toplevel/toploop.ml", line 180, characters 17-56
@@ -112,7 +112,7 @@ Called from file "toplevel/toploop.ml", line 180, characters 17-56
 
 let _ = convert_longident "A.B(C)"
 [%%expect{|
-Exception: Invalid_argument "Ppx.Longident.parse".
+Exception: Invalid_argument "Ppx.Longident.parse: \"A.B(C)\"".
 Raised at file "stdlib.ml", line 34, characters 25-45
 Called from file "test/base/test.ml", line 3, characters 15-37
 Called from file "toplevel/toploop.ml", line 180, characters 17-56