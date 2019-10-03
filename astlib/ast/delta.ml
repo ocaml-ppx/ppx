f286ed454f1dda8f99d061bd9c6c465c94d26645
Carl Eastlund <ceastlund@janestreet.com>
2019-10-03 18:20:07 -0400

    copy types inside delta

--- astlib/ast/delta.ml
+++ astlib/ast/delta.ml
@@ -1,16 +1,30 @@
 open StdLabels
 
-type ('edit, 'self) t =
-  | Remove of string
-  | Insert of int * 'self
-  | Modify of string * 'edit
+type ('key, 'insert, 'modify) edit =
+  | Remove of 'key
+  | Insert of int * 'insert
+  | Modify of 'key * 'modify
 
-type nothing = |
+type ('insert, 'modify) alist = (string, string * 'insert, 'modify) edit list
 
-type field = (Grammar.data, Grammar.field) t
-type clause = (field list, Grammar.clause) t
-type kind = (clause list, Grammar.kind) t
-type grammar = kind list
+type structural = Grammar.structural
+
+type tuple = (int, Grammar.structural, structural) edit list
+
+type record = (Grammar.structural, structural) alist
+
+type clause =
+  | Tuple of tuple
+  | Record of record
+
+type variant = (Grammar.clause, clause) alist
+
+type nominal =
+  | Alias of structural
+  | Record of record
+  | Variant of variant
+
+type grammar = (Grammar.decl, nominal) alist
 
 let modify list value ~get ~set ~f =
   let list =
