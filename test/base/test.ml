ocaml-ppx/ppxlib

lident: add test for parsing
[)] being treated as a valid identfier

Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

test/base/test.ml
@@ -113,6 +113,11 @@ let _ = convert_longident "A.B(C)"
 Exception: Invalid_argument "Ppxlib.Longident.parse: \"A.B(C)\"".
 |}]
 
+let _ = convert_longident ")"
+[%%expect{|
+- : string * longident = ("( ) )", Ppxlib.Longident.Lident ")")
+|}]
+
 let _ = Ppxlib.Code_path.(file_path @@ top_level ~file_path:"dir/main.ml")
 [%%expect{|
 - : string = "dir/main.ml"

ocaml-ppx/ppx

test/base/test.ml
@@ -112,6 +112,11 @@ let _ = convert_longident "A.B(C)"
 Exception: Invalid_argument "Ppx.Longident.parse: \"A.B(C)\"".
 |}]
 
+let _ = convert_longident ")"
+[%%expect{|
+- : string * longident = ("( ) )", Ppx.Longident.Lident ")")
+|}]
+
 let _ = Ppx.Code_path.(file_path @@ top_level ~file_path:"dir/main.ml")
 [%%expect{|
 - : string = "dir/main.ml"