4829e487590220c17d7cefd89c5b5057a971b72f
Rudi Grinberg <me@rgrinberg.com>
2019-12-12 11:53:35 +0700

    Get rid of Astlib_intf
    Move interface to Astlib.mli
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- astlib/history.mli
+++ astlib/history.mli
@@ -1,4 +1,17 @@
-include Astlib_intf.History
+type t
+
+val versioned_grammars : t -> (string * Grammar.t) list
+
+val find_grammar : t -> version:string -> Grammar.t
+
+val convert
+  :  t
+  -> 'a Ast.node
+  -> src_version:string
+  -> dst_version:string
+  -> to_node:('a -> version:string -> 'a Ast.node)
+  -> of_node:('a Ast.node -> version:string -> 'a)
+  -> 'a Ast.node
 
 type 'a conversion_function
   = 'a Ast.node
