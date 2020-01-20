b80f48a15d6c8bcdb876f23d5e4e43de62ef3f4e
Nathan Rebours <nathan.p.rebours@gmail.com>
2020-01-20 15:48:33 +0100

    Clarify Shortcuts' documentation

--- ast/cinaps/shortcuts.mli
+++ ast/cinaps/shortcuts.mli
@@ -2,12 +2,13 @@
     E.g. from [expression_desc] to [expression]. *)
 type t
 
-(** Returns true if the given record has a `_desc` field, ie if it is at the
-    end of a shortcut. *)
+(** Returns true if the given record has a [XXX_desc] field. *)
 val has_desc_field : Astlib.Grammar.record -> bool
 
 (** Returns the shortcuts for the given grammar. *)
 val from_grammar : Astlib.Grammar.t -> t
 
-(** Returns the shortcut for the given type, if any. *)
+(** Returns the shortcut for the given type, if any.
+    E.g. [shortcut t "expression_desc"] should return
+    [("expression", [("pexp_desc", ...); ("pexp_loc", ...); ...])] *)
 val shortcut : t -> string -> (string * Astlib.Grammar.record) option
