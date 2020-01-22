e666f69cbb4ebf3b0e40ed7d1a0753654b840953
Carl Eastlund <ceastlund@janestreet.com>
2020-01-22 10:54:09 -0500

    Basic tests for metaquot expressions and patterns.

---
+++ metaquot/test/test_ppx_metaquot.ml
@@ -0,0 +1,21 @@
+open! Ppx
+
+let loc = Location.none
+
+(* AST expressions *)
+let expr  = [%expr 1, 2, 3]
+let pat   = [%pat? _, 2, 3]
+let type_ = [%type: int * int * _]
+let stri  = [%stri let x = 1]
+let str   = [%str let x = 1 let y = 2]
+let sigi  = [%sigi: module M : S]
+let sig_  = [%sig: module M : S type t = unit]
+
+(* AST patterns *)
+let f_expr = function [%expr 1, 2, 3]                    -> true | _ -> false
+let f_pat  = function [%pat? _, 2, 3]                    -> true | _ -> false
+let f_type = function [%type: int * int * _]             -> true | _ -> false
+let f_stri = function [%stri let x = 1]                  -> true | _ -> false
+let f_str  = function [%str let x = 1 let y = 2]         -> true | _ -> false
+let f_sigi = function [%sigi: module M : S]              -> true | _ -> false
+let f_sig  = function [%sig: module M : S type t = unit] -> true | _ -> false
---
+++ metaquot/test/test_ppx_metaquot.mli
@@ -0,0 +1,17 @@
+open! Ppx
+
+val expr  : Ppx.expression
+val pat   : Ppx.pattern
+val type_ : Ppx.core_type
+val stri  : Ppx.structure_item
+val str   : Ppx.structure
+val sigi  : Ppx.signature_item
+val sig_  : Ppx.signature
+
+val f_expr : Ppx.expression     -> bool
+val f_pat  : Ppx.pattern        -> bool
+val f_type : Ppx.core_type      -> bool
+val f_stri : Ppx.structure_item -> bool
+val f_str  : Ppx.structure      -> bool
+val f_sigi : Ppx.signature_item -> bool
+val f_sig  : Ppx.signature      -> bool
