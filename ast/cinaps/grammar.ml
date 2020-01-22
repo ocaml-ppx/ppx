75afa514d39c7e54dfb2e95646d6b860eb4c441c
Jeremie Dimino <jeremie@dimino.org>
2020-01-22 13:42:00 +0000

    Generate polymorphic {to,of}_concrete method
    Instead of generating one function per instance of the polymorphic
    type. This makes it easier to build various things such as AST viewers
    on top of Versions.
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- ast/cinaps/grammar.ml
+++ ast/cinaps/grammar.ml
@@ -3,7 +3,7 @@ open Stdppx
 let rec string_of_ty ~internal ty =
   match (ty : Astlib.Grammar.ty) with
   | Var var -> Ml.tvar var
-  | Name name -> if internal then "Node.t" else Ml.module_name name ^ ".t"
+  | Name name -> if internal then Ml.id name else Ml.module_name name ^ ".t"
   | Bool -> "bool"
   | Char -> "char"
   | Int -> "int"
@@ -14,7 +14,8 @@ let rec string_of_ty ~internal ty =
   | Option ty -> string_of_ty ~internal ty ^ " option"
   | Tuple tuple -> string_of_tuple_type ~internal tuple
   | Instance (poly, args) ->
-    Ml.poly_inst (poly ^ ".t") ~args:(List.map args ~f:(string_of_ty ~internal))
+    let name = if poly = "node" || internal then poly else poly ^ ".t" in
+    Ml.poly_inst name ~args:(List.map args ~f:(string_of_ty ~internal))
 
 and string_of_tuple_type ~internal ?(parens = true) tuple =
   Printf.sprintf "%s%s%s"
