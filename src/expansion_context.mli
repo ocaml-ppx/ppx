0130422b13a76bf2f7b5a8efb119f8040d56e261
Jeremie Dimino <jeremie@dimino.org>
2020-05-14 16:54:03 +0100

    Detach Driver from ocaml-migrate-parsetree
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/expansion_context.mli
+++ src/expansion_context.mli
@@ -9,7 +9,7 @@ module Base : sig
       ocaml-mirgate-parsetree configuration.
   *)
   val top_level :
-    omp_config:Migrate_parsetree.Driver.config ->
+    tool_name:string ->
     file_path:string ->
     t
 
@@ -29,8 +29,10 @@ module Extension : sig
   (** Return the code path for the given context *)
   val code_path : t -> Code_path.t
 
-  (** Return the ocaml-migrate-parsetree configuration for the given expansion context *)
-  val omp_config : t -> Migrate_parsetree.Driver.config
+  (** Can be used within a ppx preprocessor to know which tool is
+      calling it ["ocamlc"], ["ocamlopt"], ["ocamldoc"], ["ocamldep"],
+      ["ocaml"], ... . *)
+  val tool_name : t -> string
 
   (** Wrap a [fun ~loc ~path] into a [fun ~ctxt] *)
   val with_loc_and_path : (loc:Location.t -> path:string -> 'a) -> (ctxt:t -> 'a)
@@ -53,7 +55,7 @@ module Deriver : sig
   val code_path : t -> Code_path.t
 
   (** Return the ocaml-migrate-parsetree configuration for the given expansion context *)
-  val omp_config : t -> Migrate_parsetree.Driver.config
+  val tool_name : t -> string
 
   (** Wrap a [fun ~loc ~path] into a [fun ~ctxt] *)
   val with_loc_and_path : (loc:Location.t -> path:string -> 'a) -> (ctxt:t -> 'a)
